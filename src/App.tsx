d8c35aa087c1e5734cc36f6220943b1f7c56598b
Kipjonto <kipindiurin0702@gmail.com>
2023-07-12 08:33:50 +0300

    points are summable

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,37 @@ import './App.css';
 
 
 const Playground = () => {
-  const EMPTY_BOARD = [ [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0]];
-
+  const EMPTY_BOARD = Array<Array<string>>(8).fill(Array<string>(8).fill(''));
   const COLUMNS = [0, 1, 2, 3, 4, 5, 6, 7];
   const ROWS = [0, 1, 2, 3, 4, 5, 6, 7];
 
-  const [player, setPlayer] = useState(1);
+  const [player, setPlayer] = useState('red');
   const [board, setBoard] = useState(EMPTY_BOARD);
   const [firstScore, setFirstScore] = useState(0);
   const [secondScore, setSecondScore] = useState(0);
   const [isPlaying, setIsPlaying] = useState(true);
+  const [points, setPoints] = useState(0);
 
   function handleClick(xIndex: number) {
     for (let i = 7; i >= 0; i--) {
-      if (board[i][xIndex] === 0 && isPlaying) {
+      if (board[i][xIndex] === '' && isPlaying) {
         let boardCopy = board;
         boardCopy[i][xIndex] = player;
-
         setBoard(boardCopy);
 
-        checkWin(i, xIndex);
+        countVerticalWin(i, xIndex);
+        countHorizontalWin(i);
+        countDiagonalWin(i, xIndex, 'LeftTop->RightBottom');
+        countDiagonalWin(i, xIndex, 'LeftBottom->RightTop');
 
-        if (player == 1) {
-          setPlayer(-1);
+        if (player === 'red') {
+          setPlayer('blue');
+        } else {
+          setPlayer('red');
         }
-        else {
-          setPlayer(1);
+
+        if (points > 0) {
+          callResult();
         }
 
         break;
@@ -43,41 +41,36 @@ const Playground = () => {
     }
   }
 
-  function checkWin(y:number, x:number) {
-    checkHorizontalWin(y);
-
-    // Checks bounds to avoid "list index out of range" error
+  function countVerticalWin(y: number, x: number) {
     if (y <= 4) {
-      // Checks vertical win
-      if (board[y][x] + board[y+1][x] + 
-          board[y+2][x] + board[y+3][x] == player * 4) {
-        callResult(player);
+      const isCommonColors = board[y][x] === board[y+1][x] && board[y][x] === board[y+2][x] 
+                                                           && board[y][x] === board[y+3][x];
+      const isPlayersColor = board[y][x] === player;
+
+      if (isCommonColors && isPlayersColor) {
+        setPoints(prev => prev + 1);
       }
     }
-
-    checkDiagonalWin(y, x, 'LeftTop->RightBottom');
-    checkDiagonalWin(y, x, 'LeftBottom->RightTop');
   }
 
-  function checkHorizontalWin(y: number) {
+  function countHorizontalWin(y: number) {
     let sum = 0;
 
     for (let i = 0; i < board[y].length; i++) {
-      if (board[y][i] === player) {
-        sum += player;
-      }
-      else {
-        sum = 0;
+      if (sum > 3 && board[y][i] != player) {
+        setPoints(prev => prev + sum - 3);
+        break;
       }
 
-      if (sum === player * 4) {
-        callResult(player);
-        break;
+      if (board[y][i] === player) {
+        sum += 1;
+      } else {
+        sum = 0;
       }
     }
   }
 
-  function checkDiagonalWin(y: number, x: number, variance: string) {
+  function countDiagonalWin(y: number, x: number, variance: string) {
     let sum = 0;
 
     let xCopy = x;
@@ -87,46 +80,44 @@ const Playground = () => {
        || (yCopy > 0 && variance === 'LeftTop->RightBottom'))) {
       xCopy--;
       
-      if (variance === 'LeftBottom->RightTop'){
+      if (variance === 'LeftBottom->RightTop') {
         yCopy++;
-      }
-      else {
+      } else {
         yCopy--;
       }
     }
 
-    while (yCopy < 8 && yCopy >= 0 && xCopy < 8){
-      if (board[yCopy][xCopy] == player) {
-        sum++;
+    while (yCopy < 8 && yCopy >= 0 && xCopy < 8) {
+      if (sum > 3 && board[yCopy][xCopy] != player) {
+        setPoints(prev => prev + sum - 3);
+        break;
       }
-      else {
+
+      if (board[yCopy][xCopy] === player) {
+        sum++;
+      } else {
         sum = 0;
       }
 
       xCopy++;
 
-      if (variance === 'LeftBottom->RightTop'){
+      if (variance === 'LeftBottom->RightTop') {
         yCopy--;
-      }
-      else {
+      } else {
         yCopy++;
-      }
-
-      if (sum === 4) {
-        callResult(player);
-        break;
       }
     }
   }
 
-  function callResult(player: number){
-    if (player === 1) {
-      setFirstScore(prev => prev + 1);
-    } 
-    else {
-      setSecondScore(prev => prev + 1);
+  function callResult() {
+    if (player === 'red') {
+      setFirstScore(prev => prev + points);
+    } else {
+      setSecondScore(prev => prev + points);
     }
+
     setIsPlaying(false);
+    setPoints(0);
   }
 
   function restartGame() {
@@ -135,49 +126,68 @@ const Playground = () => {
   }
 
   const CellsContainer = () => {
+    interface Cell {
+      y: number;
+      x: number;
+    }
+
+    const Cell = (props: Cell) => {
+      return (
+        <div className='cell' style={{backgroundColor: `${board[props.y][props.x]}`}} />
+      );
+    }
+
     return (
       <div className='cell-container'>
         {ROWS
-          .map( yIndex => <div className='cells-row' key={yIndex}>
-            {COLUMNS
-              .map( xIndex => <div className='cell' key={xIndex}
-                    style={ !(board[yIndex][xIndex] === 0) 
-                            ? board[yIndex][xIndex] === 1 
-                            ? { backgroundColor: 'red' } 
-                            : { backgroundColor: 'blue' }
-                            : {} }/>
-            )} </div>
-        )} </div>
+          .map( y => 
+            <div className='cells-row' key={y}>
+              {COLUMNS
+                .map( x => <Cell y={y} x={x} key={x} /> )
+              } 
+          </div>
+        )} 
+      </div>
     );
   }
 
   const ButtonsArea = () => {
+    interface Button {
+      x: number;
+    }
+
+    const Button = (props: Button) => {
+      return (
+        <div className='buttons' onClick={() => handleClick(props.x)}>▲</div>
+      );
+    }
+
     return (
       <div className='buttons-row'>
       { COLUMNS
-          .map( el => <div className='flex-center-center buttons' key={el} 
-                           onClick={() => handleClick(el)}>▲</div>
-      )} </div>
+          .map( x => <Button x={x} key={x} /> )
+      } 
+      </div>
     );
   }
 
   const ScoreTable = () => {
     return (
       <div className='players-display'>
-        <h1 className='flex-center-center score' style={player === 1 ? {backgroundColor: 'red'} : {color: 'red'}}>
+        <h1 className='score' style={player === 'red' ? {backgroundColor: 'red'} : {color: 'red'}}>
           {firstScore}</h1>
-        <h1 className='flex-center-center score' style={player === -1 ? {backgroundColor: 'blue'} : {color: 'blue'}}>
+        <h1 className='score' style={player === 'blue' ? {backgroundColor: 'blue'} : {color: 'blue'}}>
           {secondScore}</h1>
       </div>
     );
   }
 
   return (
-    <div className='flex-center-center canvas'>
+    <div className='canvas'>
       <CellsContainer />
       <ButtonsArea />
       <ScoreTable />
-      <div className='flex-center-center button--restart' onClick={restartGame}>⭮</div>
+      <div className='button--restart' onClick={restartGame}>⭮</div>
     </div>
   );
 }
